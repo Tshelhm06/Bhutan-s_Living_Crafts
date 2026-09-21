2991836aa64cf47d5e28b570afca14b4cdd690ac
Tshelhm06 <lhamo2046@gmail.com>
2026-09-21 13:55:12 +0600

    Initial Commit

---
+++ Lab6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab6
+//
+//  Created by iMac16 on 9/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab6/Lab6App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab6App.swift
+//  Lab6
+//
+//  Created by iMac16 on 9/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
